32f4c9996c0b0f2e155db7fbd3a631c93445bdc5
Leigh Whitehead <leigh.howard.whitehead@cern.ch>
2018-06-04 09:46:13 -0500

    Add new class to encapsulate the information from each beam instrument

---
+++ dunesim/EventGenerator/ProtoDUNEbeamDataProducts/ProtoDUNEBeamInstrument.cxx
@@ -0,0 +1,78 @@
+#include <iostream>
+#include "dune/EventGenerator/ProtoDUNEbeamDataProducts/ProtoDUNEBeamInstrument.h"
+
+namespace sim{
+
+  //--------------------------constructors-----------------------------------------------
+  ProtoDUNEBeamInstrument::ProtoDUNEBeamInstrument(){
+    fInstrumentName = "default";
+    fX = 0.0;
+    fY = 0.0;
+    fZ = 0.0;
+    fPx = 0.0;
+    fPy = 0.0;
+    fPz = 0.0;
+    fPDGid = 0;
+    fEventID = 0;
+    fTrackID = 0;
+  }
+
+  //-------------------------------default destructor------------------------------------------
+  ProtoDUNEBeamInstrument::~ProtoDUNEbeamsim(){
+
+  }
+
+  ProtoDUNEBeamInstrument::ProtoDUNEBeamInstrument(std::string name,
+      Float_t x,
+      Float_t y,
+      Float_t z,
+      Float_t Px,
+      Float_t Py,
+      Float_t Pz,
+      Int_t PDGid,
+      Int_t EventID,
+      Int_t TrackID
+      ){
+    fInstrumentName = name;
+    fX = x; fY = y; fZ = z;
+    fPx = Px; fPy = Py; fPz = Pz;
+    fPDGid = PDGig;
+    fEventID = EventID;
+    fTrackID = TrackID;
+  }
+
+  /// Vector-based constructor
+  ProtoDUNEBeamInstrument::ProtoDUNEBeamInstrument(std::string name,
+      std::vector<Float_t> position,
+      std::vector<Float_t> momentum,
+      Int_t PDGid,
+      Int_t EventID,
+      Int_t TrackID
+      ){
+    fInstrumentName = name;
+    fX = position[0]; fY = position[1]; fZ = position[2];
+    fPx = momentum[0]; fPy = momentum[1]; fPz = momentum[2];
+    fPDGid = PDGig;
+    fEventID = EventID;
+    fTrackID = TrackID;
+  }
+
+  // TVector3-based constructor
+  ProtoDUNEBeamInstrument::ProtoDUNEBeamInstrument(std::string name,
+      TVector3 position,
+      TVector3 momentum,
+      Int_t PDGid,
+      Int_t EventID,
+      Int_t TrackID
+      ){
+    fInstrumentName = name;
+    fX = position.X(); fY = position.Y(); fZ = position.Z();
+    fPx = momentum.X(); fPy = momentum.Y(); fPz = momentum.Z();
+    fPDGid = PDGig;
+    fEventID = EventID;
+    fTrackID = TrackID;
+
+  }
+
+}
+
---
+++ dunesim/EventGenerator/ProtoDUNEbeamDataProducts/ProtoDUNEBeamInstrument.h
@@ -0,0 +1,96 @@
+// ProtoDUNEBeamInstrument.h
+//
+// Leigh Whitehead
+// leigh.howard.whitehead@cern.ch
+// June 2018
+//
+// This class contains all of the information from a given
+// simulated beam line component
+
+#ifndef PROTODUNEBEAMINSTRUMENT_H
+#define PROTODUNEBEAMINSTRUMENT_H
+
+#include <string>
+#include <vector>
+#include "TVector3.h"
+
+namespace sim {
+  class ProtoDUNEBeamInstrument  {
+
+    public:
+      ProtoDUNEBeamInstrument(); //constructor
+      ~ProtoDUNEBeamInstrument(); //destructor
+
+    private:
+
+      std::string fInstrumentName;
+
+      Float_t fX;
+      Float_t fY;
+      Float_t fZ;
+      Float_t fPx;
+      Float_t fPy;
+      Float_t fPz;
+      Int_t fPDGid;
+      Int_t fEventID;
+      Int_t fTrackID;
+
+    public:
+
+      ProtoDUNEBeamInstrument(std::string name,
+        Float_t x,
+        Float_t y,
+        Float_t z,
+        Float_t Px,
+        Float_t Py,
+        Float_t Pz,
+        Int_t PDGid,
+        Int_t EventID,
+        Int_t TrackID
+      );
+
+      /// Vector-based constructor
+      ProtoDUNEBeamInstrument(std::string name, 
+        std::vector<Float_t> position,
+        std::vector<Float_t> momentum,
+        Int_t PDGid,
+        Int_t EventID,
+        Int_t TrackID
+      );      
+
+      // TVector3-based constructor
+      ProtoDUNEBeamInstrument(std::string name, 
+        TVector3 position,
+        TVector3 momentum,
+        Int_t PDGid,
+        Int_t EventID,
+        Int_t TrackID
+      );      
+
+      std::string GetInstrumentName() const {return fInstrumentName;};
+      Float_t GetX() const {return fX;};
+      Float_t GetY() const {return fY;};
+      Float_t GetY() const {return fZ;};
+      Float_t GetPx() const {return fPx;};
+      Float_t GetPy() const {return fPy;};
+      Float_t GetPz() const {return fPz;};
+      Int_t GetPDGid() const {return fPDGid;};
+      Int_t GetEventID() const {return fEventID;}; 
+      Int_t GetTrackID() const {return fTrackID;};
+
+      void SetInstrumentName(std::string name) {fInstrumentName = name;};
+      void SetX(Float_t val) {fX = val;};
+      void SetY(Float_t val) {fY = val;};
+      void SetZ(Float_t val) {fZ = val;};
+      void SetPx(Float_t val) {fPx = val;};
+      void SetPy(Float_t val) {fPy = val;};
+      void SetPz(Float_t val) {fPz = val;};
+      void SetPDGid(Int_t val) {fPDGid = val;};
+      void SetEventID(Int_t val) {fEventID = val;};
+      void SetTrackID(Int_t val) {fTrackID = val;};
+
+  };
+}
+
+////////////////////////////////////////////////////////////////////////
+#endif // PROTODUNEBEAMINSTRUMENT_H
