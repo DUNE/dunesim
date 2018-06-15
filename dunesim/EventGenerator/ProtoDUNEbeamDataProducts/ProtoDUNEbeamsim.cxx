6a342eda596ae297eb2bdad2800d4818afab9ee4
Pablo Fernandez Menendez <pablofer@dunebuild01.fnal.gov>
2018-06-15 04:10:56 -0500

    cleaning protoDUNEbeamDataProducts

--- dunesim/EventGenerator/ProtoDUNEbeamDataProducts/ProtoDUNEbeamsim.cxx
+++ dunesim/EventGenerator/ProtoDUNEbeamDataProducts/ProtoDUNEbeamsim.cxx
@@ -17,144 +17,11 @@
 namespace sim{
 
   //--------------------------constructors-----------------------------------------------
-  ProtoDUNEbeamsim::ProtoDUNEbeamsim(){
-/*     BPROFEXT_x=0;
-     BPROFEXT_y=0;
-     BPROFEXT_z=0;
-     BPROFEXT_Px=0;
-     BPROFEXT_Py=0;
-     BPROFEXT_Pz=0;
-     BPROFEXT_PDGid=0;
-     BPROFEXT_EventID=0;
-     BPROFEXT_TrackID=0;
-     BPROF4_x=0;
-     BPROF4_y=0;
-     BPROF4_z=0;
-     BPROF4_Px=0;
-     BPROF4_Py=0;
-     BPROF4_Pz=0;
-     BPROF4_PDGid=0;
-     BPROF4_EventID=0;
-     BPROF4_TrackID=0;
-     TOF1_t=0;
-     TOF1_x=0;
-     TOF1_y=0;
-     TOF1_z=0;
-     TOF1_Px=0;
-     TOF1_Py=0;
-     TOF1_Pz=0;
-     TOF1_PDGid=0;
-     TRIG2_x=0;
-     TRIG2_y=0;
-     TRIG2_z=0;
-     TRIG2_Px=0;
-     TRIG2_Py=0;
-     TRIG2_Pz=0;
-     TRIG2_PDGid=0;
-     TRIG2_EventID=0;
-     TRIG2_TrackID=0;
-     NP04FieldCage_x=0;
-     NP04FieldCage_y=0;
-     NP04FieldCage_z=0;
-     NP04FieldCage_Px=0;
-     NP04FieldCage_Py=0;
-     NP04FieldCage_Pz=0;
-     NP04FieldCage_EventID=0;
-     NP04FieldCage_TrackID=0; */
-  }
+  ProtoDUNEbeamsim::ProtoDUNEbeamsim(){}
   
   //-------------------------------default destructor------------------------------------------
   ProtoDUNEbeamsim::~ProtoDUNEbeamsim(){ }
 
-
-  //------------------------alternate constructor-------------------------------------------------
-/*  ProtoDUNEbeamsim::ProtoDUNEbeamsim(
-    Float_t         vBPROFEXT_x,
-    Float_t         vBPROFEXT_y,
-    Float_t         vBPROFEXT_z,
-    Float_t         vBPROFEXT_Px,
-    Float_t         vBPROFEXT_Py,
-    Float_t         vBPROFEXT_Pz,
-    Float_t         vBPROFEXT_PDGid,
-    Float_t         vBPROFEXT_EventID,
-    Float_t         vBPROFEXT_TrackID,
-    Float_t         vBPROF4_x,
-    Float_t         vBPROF4_y,
-    Float_t         vBPROF4_z,
-    Float_t         vBPROF4_Px,
-    Float_t         vBPROF4_Py,
-    Float_t         vBPROF4_Pz,
-    Float_t         vBPROF4_PDGid,
-    Float_t         vBPROF4_EventID,
-    Float_t         vBPROF4_TrackID,
-    Float_t         vTOF1_t,
-    Float_t         vTOF1_x,
-    Float_t         vTOF1_y,
-    Float_t         vTOF1_z,
-    Float_t         vTOF1_Px,
-    Float_t         vTOF1_Py,
-    Float_t         vTOF1_Pz,
-    Float_t         vTOF1_PDGid,
-    Float_t         vTRIG2_x,
-    Float_t         vTRIG2_y,
-    Float_t         vTRIG2_z,
-    Float_t         vTRIG2_Px,
-    Float_t         vTRIG2_Py,
-    Float_t         vTRIG2_Pz,
-    Float_t         vTRIG2_EventID,
-    Float_t         vTRIG2_TrackID,
-    Float_t         vNP04FieldCage_x,
-    Float_t         vNP04FieldCage_y,
-    Float_t         vNP04FieldCage_z,
-    Float_t         vNP04FieldCage_Px,
-    Float_t         vNP04FieldCage_Py,
-    Float_t         vNP04FieldCage_Pz,
-    Float_t         vNP04FieldCage_EventID,
-    Float_t         vNP04FieldCage_TrackID){
-     BPROFEXT_x=vBPROFEXT_x;
-     BPROFEXT_y=vBPROFEXT_y;
-     BPROFEXT_z=vBPROFEXT_z;
-     BPROFEXT_Px=vBPROFEXT_Px;
-     BPROFEXT_Py=vBPROFEXT_Py;
-     BPROFEXT_Pz=vBPROFEXT_Pz;
-     BPROFEXT_PDGid=vBPROFEXT_PDGid;
-     BPROFEXT_EventID=vBPROFEXT_EventID;
-     BPROFEXT_TrackID=vBPROFEXT_TrackID;
-     BPROF4_x=vBPROF4_x;
-     BPROF4_y=vBPROF4_y;
-     BPROF4_z=vBPROF4_z;
-     BPROF4_Px=vBPROF4_Px;
-     BPROF4_Py=vBPROF4_Py;
-     BPROF4_Pz=vBPROF4_Pz;
-     BPROF4_PDGid=vBPROF4_PDGid;
-     BPROF4_EventID=vBPROF4_EventID;
-     BPROF4_TrackID=vBPROF4_TrackID;
-     TOF1_t=vTOF1_t;
-     TOF1_x=vTOF1_x;
-     TOF1_y=vTOF1_y;
-     TOF1_z=vTOF1_z;
-     TOF1_Px=vTOF1_Px;
-     TOF1_Py=vTOF1_Py;
-     TOF1_Pz=vTOF1_Pz;
-     TOF1_PDGid=vTOF1_PDGid;
-     TRIG2_x=vTRIG2_x;
-     TRIG2_y=vTRIG2_y;
-     TRIG2_z=vTRIG2_z;
-     TRIG2_Px=vTRIG2_Px;
-     TRIG2_Py=vTRIG2_Py;
-     TRIG2_Pz=vTRIG2_Pz;
-     TRIG2_EventID=vTRIG2_EventID;
-     TRIG2_TrackID=vTRIG2_TrackID;
-     NP04FieldCage_x=vNP04FieldCage_x;
-     NP04FieldCage_y=vNP04FieldCage_y;
-     NP04FieldCage_z=vNP04FieldCage_z;
-     NP04FieldCage_Px=vNP04FieldCage_Px;
-     NP04FieldCage_Py=vNP04FieldCage_Py;
-     NP04FieldCage_Pz=vNP04FieldCage_Pz;
-     NP04FieldCage_EventID=vNP04FieldCage_EventID;
-     NP04FieldCage_TrackID=vNP04FieldCage_TrackID;
-  }
-*/
   // Leigh - I have encapsulated each beam instrument into a ProtoDUNEBeamInstrument object.
   // The below provides the interface for these.
   void ProtoDUNEbeamsim::AddInstrument(ProtoDUNEBeamInstrument newInst){
@@ -193,7 +60,6 @@ namespace sim{
 
     return temp;
   }
-
 }// namespace
 
 
--- dunesim/EventGenerator/ProtoDUNEbeamDataProducts/ProtoDUNEbeamsim.h
+++ dunesim/EventGenerator/ProtoDUNEbeamDataProducts/ProtoDUNEbeamsim.h
@@ -33,109 +33,17 @@ namespace sim {
     
   private:
 
-    //values I need to contain
-/*    Float_t         BPROFEXT_x;
-    Float_t         BPROFEXT_y;
-    Float_t         BPROFEXT_z;
-    Float_t         BPROFEXT_Px;
-    Float_t         BPROFEXT_Py;
-    Float_t         BPROFEXT_Pz;
-    Float_t         BPROFEXT_PDGid;
-    Float_t         BPROFEXT_EventID;
-    Float_t         BPROFEXT_TrackID;
-    Float_t         BPROF4_x;
-    Float_t         BPROF4_y;
-    Float_t         BPROF4_z;
-    Float_t         BPROF4_Px;
-    Float_t         BPROF4_Py;
-    Float_t         BPROF4_Pz;
-    Float_t         BPROF4_PDGid;
-    Float_t         BPROF4_EventID;
-    Float_t         BPROF4_TrackID;
-    Float_t         TOF1_t;
-    Float_t         TOF1_x;
-    Float_t         TOF1_y;
-    Float_t         TOF1_z;
-    Float_t         TOF1_Px;
-    Float_t         TOF1_Py;
-    Float_t         TOF1_Pz;
-    Float_t         TOF1_PDGid;
-    Float_t         TRIG2_x;
-    Float_t         TRIG2_y;
-    Float_t         TRIG2_z;
-    Float_t         TRIG2_Px;
-    Float_t         TRIG2_Py;
-    Float_t         TRIG2_Pz;
-    Float_t         TRIG2_PDGid;
-    Float_t         TRIG2_EventID;
-    Float_t         TRIG2_TrackID;
-    Float_t         NP04FieldCage_x;
-    Float_t         NP04FieldCage_y;
-    Float_t         NP04FieldCage_z;
-    Float_t         NP04FieldCage_Px;
-    Float_t         NP04FieldCage_Py;
-    Float_t         NP04FieldCage_Pz;
-    Float_t         NP04FieldCage_PDGid;
-    Float_t         NP04FieldCage_EventID;
-    Float_t         NP04FieldCage_TrackID; */
-   
     // Leigh: I think it would make much more sense to store a ProtoDUNEBeamInstrument object
     // for each of the instruments
     std::vector<ProtoDUNEBeamInstrument> fAllInstruments;
 
   public:
 
-    /*ProtoDUNEbeamsim(  Float_t         BPROFEXT_x,
-                         Float_t         BPROFEXT_y,
-                         Float_t         BPROFEXT_z,
-                         Float_t         BPROFEXT_Px,
-                         Float_t         BPROFEXT_Py,
-                         Float_t         BPROFEXT_Pz,
-                         Float_t         BPROFEXT_PDGid,
-                         Float_t         BPROFEXT_EventID,
-                         Float_t         BPROFEXT_TrackID,
-			 Float_t         BPROF4_x,
-			 Float_t         BPROF4_y,
-			 Float_t         BPROF4_z,
-			 Float_t         BPROF4_Px,
-			 Float_t         BPROF4_Py,
-			 Float_t         BPROF4_Pz,
-			 Float_t         BPROF4_PDGid,
-			 Float_t         BPROF4_EventID,
-			 Float_t         BPROF4_TrackID,
-			 Float_t         TOF1_t,
-			 Float_t         TOF1_x,
-                         Float_t         TOF1_y,
-                         Float_t         TOF1_z,
-                         Float_t         TOF1_Px,
-                         Float_t         TOF1_Py,
-                         Float_t         TOF1_Pz,
-                         Float_t         TOF1_PDGid,
-			 Float_t         TRIG2_x,
-			 Float_t         TRIG2_y,
-			 Float_t         TRIG2_z,
-			 Float_t         TRIG2_Px,
-			 Float_t         TRIG2_Py,
-			 Float_t         TRIG2_Pz,
-			 Float_t         TRIG2_EventID,
-			 Float_t         TRIG2_TrackID,
-			 Float_t         NP04FieldCage_x,
-                         Float_t         NP04FieldCage_y,
-                         Float_t         NP04FieldCage_z,
-                         Float_t         NP04FieldCage_Px,
-                         Float_t         NP04FieldCage_Py,
-                         Float_t         NP04FieldCage_Pz,
-                         Float_t         NP04FieldCage_EventID,
-                         Float_t         NP04FieldCage_TrackID);*/
-
     void AddInstrument(ProtoDUNEBeamInstrument newInst);
     ProtoDUNEBeamInstrument GetInstrument(std::string name) const;
     unsigned short NInstruments() {return fAllInstruments.size();};
     
   };
-  
-  
 }
-
 ////////////////////////////////////////////////////////////////////////
 #endif // PROTODUNESIM_H
