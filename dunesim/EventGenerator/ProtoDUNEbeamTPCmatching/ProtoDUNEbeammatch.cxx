42bec943edcd5bf812c1606a90d364f605bba44e
Pablo Fernandez Menendez <pablofer@dunebuild01.fnal.gov>
2018-06-15 04:32:08 -0500

    defining protoDUNEbeamTPCmatching

---
+++ dunesim/EventGenerator/ProtoDUNEbeamTPCmatching/ProtoDUNEbeammatch.cxx
@@ -0,0 +1,34 @@
+/////////////////////////////////////////////////////////
+/////////// ProtoDUNEbeammatch.cxx //////////////////////
+/////////// Pablo F. pablo.fer@cern.ch //////////////////
+/////////// June 2018 ///////////////////////////////////
+/////////////////////////////////////////////////////////
+
+#include <iostream>
+#include <fstream>
+#include "dune/EventGenerator/ProtoDUNEbeamTPCMatching/ProtoDUNEbeammatch.h"
+#include <string.h>
+#include <time.h>
+#include <cmath>
+#include <iomanip>
+
+#include "messagefacility/MessageLogger/MessageLogger.h"
+
+namespace match{
+
+// constructor
+  ProtoDUNEbeammatch::ProtoDUNEbeammatch(){}
+
+// destructor
+  ProtoDUNEbeammatch::~ProtoDUNEbeammatch(){}
+
+// Get monitors for direction (BPROFEXT and BPROF4) and ToF (TOF1 and TRIG2)
+  
+
+
+
+
+
+
+
+}//namespace
---
+++ dunesim/EventGenerator/ProtoDUNEbeamTPCmatching/ProtoDUNEbeammatch.h
@@ -0,0 +1,42 @@
+/////////////////////////////////////////////////////////
+///////////// ProtoDUNEbeammatch.h //////////////////////
+///////////// Pablo F. pablo.fer@cern.ch ////////////////
+///////////// June 2018 /////////////////////////////////
+/////////////////////////////////////////////////////////
+
+#ifndef PROTODUNEBEAMMATCH_H
+#define PROTODUNEBEAMMATCH_H
+
+#include <iostream>
+#include <map>
+#include <string>
+#include <vector>
+#include "TChain.h"
+#include "TFile.h"
+#include "TTree.h"
+#include "TVector3.h"
+#include "TDirectory.h"
+#include "TStyle.h"
+#include "TMath.h"
+
+//#include "dune/EventGenerator/ProtoDUNEbeamTPCmatching/ProtoDUNEBeamDirection.h"
+//#include "dune/EventGenerator/ProtoDUNEbeamTPCmatching/ProtoDUNEBeamTof.h"
+
+namespace match {
+ class ProtoDUNEbeamMatch  {
+
+  public:
+    ProtoDUNEbeammatch(); //constructor
+
+    ~ProtoDUNEbeammatch(); //destructor
+
+    private:
+
+
+
+
+
+
+}//namespace
+#endif // PROTODUNEMATCH_H
+
