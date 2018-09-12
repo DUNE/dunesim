911f1d58dd9fe8ccccb91f525529c892e84b1ee4
Christoph Alt <calt@dunegpvm03.fnal.gov>
2018-09-12 14:48:11 -0500

    Supporting drift in y direction

--- dunesim/DetSim/Service/DPhaseFixedDetPedestalService_service.cc
+++ dunesim/DetSim/Service/DPhaseFixedDetPedestalService_service.cc
@@ -14,12 +14,16 @@ DPhaseFixedDetPedestalService(const fhicl::ParameterSet& pset, art::ActivityRegi
 
 DPhaseFixedDetPedestalService::
 DPhaseFixedDetPedestalService(const fhicl::ParameterSet& pset) {
+  m_PedMeanX    = pset.get<float>("PedMeanX");
   m_PedMeanY    = pset.get<float>("PedMeanY");
   m_PedMeanZ    = pset.get<float>("PedMeanZ");
+  m_PedRmsX     = pset.get<float>("PedRmsX");
   m_PedRmsY     = pset.get<float>("PedRmsY");
   m_PedRmsZ     = pset.get<float>("PedRmsZ");
+  m_PedMeanErrX = pset.get<float>("PedMeanErrX");
   m_PedMeanErrY = pset.get<float>("PedMeanErrY");
   m_PedMeanErrZ = pset.get<float>("PedMeanErrZ");
+  m_PedRmsErrX  = pset.get<float>("PedRmsErrX");
   m_PedRmsErrY  = pset.get<float>("PedRmsErrY");
   m_PedRmsErrZ  = pset.get<float>("PedRmsErrZ");
 }
@@ -27,8 +31,9 @@ DPhaseFixedDetPedestalService(const fhicl::ParameterSet& pset) {
 //**********************************************************************
 
 float DPhaseFixedDetPedestalService::PedMean(raw::ChannelID_t ch) const {
-  if      ( m_hgeo->View(ch) == geo::kZ ) return m_PedMeanZ;
+  if      ( m_hgeo->View(ch) == geo::kX ) return m_PedMeanX;
   else if ( m_hgeo->View(ch) == geo::kY ) return m_PedMeanY;
+  else if ( m_hgeo->View(ch) == geo::kZ ) return m_PedMeanZ;
   return -999.0;
 }
 
@@ -41,12 +46,18 @@ float DPhaseFixedDetPedestalService::PedRms(raw::ChannelID_t ch) const {
 //**********************************************************************
 
 float DPhaseFixedDetPedestalService::PedMeanErr(raw::ChannelID_t ch) const {
-  return m_PedMeanErrZ;
+  if      ( m_hgeo->View(ch) == geo::kX ) return m_PedMeanErrX;
+  else if ( m_hgeo->View(ch) == geo::kY ) return m_PedMeanErrY;
+  else if ( m_hgeo->View(ch) == geo::kZ ) return m_PedMeanErrZ;
+  return -999.0;
 }
 
 //**********************************************************************
 
 float DPhaseFixedDetPedestalService::PedRmsErr(raw::ChannelID_t ch) const {
+  if      ( m_hgeo->View(ch) == geo::kX ) return m_PedRmsErrX;
+  else if ( m_hgeo->View(ch) == geo::kY ) return m_PedRmsErrY;
+  else if ( m_hgeo->View(ch) == geo::kZ ) return m_PedRmsErrZ;
   return m_PedRmsErrZ;
 }
 
