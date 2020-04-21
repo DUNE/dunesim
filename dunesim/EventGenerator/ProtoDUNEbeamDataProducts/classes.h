ff7b831575660e43dfea14cedc025751b6b8a201
Thomas Junk <trj@fnal.gov>
2020-04-21 16:49:07 -0500

    fix comment

--- dunesim/EventGenerator/ProtoDUNEbeamDataProducts/classes.h
+++ dunesim/EventGenerator/ProtoDUNEbeamDataProducts/classes.h
@@ -13,6 +13,6 @@
 #include "nusimdata/SimulationBase/MCTruth.h"
 
 // DuneTPC includes
-//#include "/dune/EventGenerator/ProtoDUNEbeamDataProducts/ProtoDUNEbeamsim.h"
+//#include "dune/EventGenerator/ProtoDUNEbeamDataProducts/ProtoDUNEbeamsim.h"
 #include "dune/EventGenerator/ProtoDUNEbeamDataProducts/ProtoDUNEbeamsim.h"
 #include "dune/EventGenerator/ProtoDUNEbeamDataProducts/ProtoDUNEBeamInstrument.h"
