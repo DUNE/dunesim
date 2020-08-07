16d8a991f8341fdced1c7e53bd3ae4ea50307350
Jacob Calcutt <calcuttj@msu.edu>
2020-08-07 15:35:51 -0500

    Adding some comments describing the technique/plan for the data driven module

--- dunesim/EventGenerator/PDSPDataDrivenBeam_module.cc
+++ dunesim/EventGenerator/PDSPDataDrivenBeam_module.cc
@@ -88,6 +88,21 @@ void PDSPDataDrivenBeam::beginJob() {
   //These will be a 1D distributions in momentum for each
   //4D monitor bin (x,y for up/downstream)
   //Stored in directories according to their particle type
+  //
+  //
+  //Thinking of moving this to the following variables:
+  //
+  //horiz/vert fibers in last XBPF
+  //Px, Py, Pz -- or P, theta, phi (I like this more I think)
+  //
+  //Where the directions are taken from the reconstructed
+  //tracks from the last 2 XBPFs.
+  //
+  //The momentum is a bit tricky: we need to sample in 
+  //reco space, but generate in true space. We'll have to 
+  //unfold from the reco momentum in the spectrometer to 
+  //the true energy (taken from nominal MC) at the generation 
+  //point (what is called the face in the beam sim files I think)
   for (size_t i = 0; i < fParticleTypes.size(); ++i) {
     std::cout << "Fetching directory for " << fParticleTypes[i] << std::endl;
     TDirectory * current_dir = (TDirectory*)fInputFile->Get(fParticleTypes[i].c_str());
@@ -101,6 +116,27 @@ void PDSPDataDrivenBeam::beginJob() {
     }
   }
 
+  //Will need to throw some random numbers
+  //Flat in reco momentum
+  //Flat in theta, phi, vert/horiz fibers
+  //     --- Establish valid ranges?
+  //
+  //Then for each of these, throw flat(0,1) 
+  //and check against PDF in that 5D bin
+  //
+  //
+  //If rejected, go back to throwing in phase space
+  //and check against PDF again. This will fill the 
+  //phase space according to the PDF
+  //
+  //
+  //
+  //I think we can do this all in the beginJob step, 
+  //where we throw all of the numbers until we get 
+  //N events, where N is a fcl parameter describing how
+  //many events we would like to generator. Save the 
+  //kinematics + position for these N events and then use
+  //in the produce function to create the events. 
 }
 
 DEFINE_ART_MODULE(PDSPDataDrivenBeam)
