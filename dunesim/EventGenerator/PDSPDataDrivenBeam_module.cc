4ca466dff611e6599fce7276118b197bd8da8446
Jacob Calcutt <calcuttj@msu.edu>
2020-08-10 16:18:20 -0500

    Implemented bulk of datadriven beam module. Takes in THnSparseD objects from an input file (generated by make_THn_beam_input.py) and uses those as PDFs for various particle types --> Throws some random numbers to get PDG type, momentum, and positions in last 2 beam monitors. Then throws a random number to check against the value of the 5D PDF at this point. Continues sampling until (by finding another point and throwing another random number) successful. Also, starting to implement the parts that actually create the true particles and stores them to MCTruth/in the output file. Finally, made a quick edit to BeamEvent_module to reformat how a comment was situated in a few lines

--- dunesim/EventGenerator/PDSPDataDrivenBeam_module.cc
+++ dunesim/EventGenerator/PDSPDataDrivenBeam_module.cc
@@ -16,30 +16,21 @@
 #include "canvas/Utilities/InputTag.h"
 #include "fhiclcpp/ParameterSet.h"
 #include "messagefacility/MessageLogger/MessageLogger.h"
+#include "art_root_io/TFileService.h"
+#include "nusimdata/SimulationBase/MCTruth.h"
+#include "nusimdata/SimulationBase/MCParticle.h"
 
 #include <memory>
+#include <map>
 
 #include "TFile.h"
+#include "THnSparse.h"
+#include "TVectorD.h"
+#include "TRandom3.h"
+#include "TTree.h"
+#include "TH1D.h"
 
 class PDSPDataDrivenBeam;
-
-/*
-struct MonitorRegion {
-  int upstream_low;
-  int upstream_high;
-  int downstream_low;
-  int downstream_high;
-
-  MonitorRegion(int up_low, int up_high, int down_low, int down_high)
-    : upstream_low(up_low), upstream_high(up_high),
-      downtream_low(down_low), downstream_high(down_high) {};
-
-  check(int up, int down) {
-    return ((upstream_low <= up && up <= upstream_high) &&
-            (downstream_low <= down && down <= downstream_high));
-  };
-};
-*/
 
 class PDSPDataDrivenBeam : public art::EDProducer {
 public:
@@ -63,35 +54,114 @@ private:
   TFile* fInputFile = 0x0;
   std::string fFileName;
   std::vector<std::string> fParticleTypes;
+  int fNGenerate;
+  int fSeed;
+  TRandom3 fRNG;
+  bool fVerbose;
 
+  std::map<std::string, THnSparseD *> fPDFs;
+  std::map<std::string, int> fParticleNums;
+  double fTotalParticles = 0.;
+  std::map<std::string, double> fParticleFracs;
+  int fCurrentEvent = 0;
+
+  std::vector<int> fRandPDG;
+  std::vector<double> fRandMomentum;
+  std::vector<double> fRandHUpstream, fRandVUpstream;
+  std::vector<double> fRandHDownstream, fRandVDownstream;
+
+  TTree * fOutputTree;
+  int fOutputPDG;
+  double fOutputMomentum;
+  double fOutputHUpstream, fOutputVUpstream;
+  double fOutputHDownstream, fOutputVDownstream;
+
+  std::map<std::string, int> fNameToPDG = {
+    {"Protons", 2212},
+    {"Pions", 211},
+    {"Electrons", -11},
+  };
+
+  TVector3 fBMBasisX = TVector3(1.,0.,0.);
+  TVector3 fBMBasisY = TVector3(0.,1.,0.);
+  TVector3 fBMBasisZ = TVector3(0.,0.,1.);
+
+  double fRotateMonitorXZ, fRotateMonitorYZ;
+  double fNP04FrontZ, fBeamX, fBeamY, fBeamZ;
+  double fUpstreamZ, fDownstreamZ, fGeneratorZ;
+
+
+  void Convert(double input_point[5], double minima[5],
+               double maxima[5], double output_point[5]);
+
+  void GenerateTrueEvent(simb::MCTruth &mcTruth);
+  //TLorentzVector MakeMomentumVector();
+  TLorentzVector MakePositionVector();
+  TVector3 ConvertProfCoordinates(double x, double y, double z, double zOffset);
+  void BeamMonitorBasisVectors();
+  void RotateMonitorVector(TVector3 & vec);
 };
 
 
-PDSPDataDrivenBeam::PDSPDataDrivenBeam(
-    fhicl::ParameterSet const& p)
+PDSPDataDrivenBeam::PDSPDataDrivenBeam(fhicl::ParameterSet const& p)
   : EDProducer{p},
-    fFileName(p.get<std::string>("FileName")) {
+    fFileName(p.get<std::string>("FileName")),
+    fNGenerate(p.get<int>("NGenerate")),
+    fSeed(p.get<int>("Seed")),
+    fRNG(fSeed),
+    fRotateMonitorXZ(p.get<double>("RotateMonitorXZ")),
+    fRotateMonitorYZ(p.get<double>("RotateMonitorYZ")),
+    fNP04FrontZ(p.get<double>("NP04FrontZ")),
+    fBeamX(p.get<double>("BeamX")),
+    fBeamY(p.get<double>("BeamY")),
+    fBeamZ(p.get<double>("BeamZ")),
+    fUpstreamZ(p.get<double>("UpstreamZ")),
+    fDownstreamZ(p.get<double>("DownstreamZ")),
+    fGeneratorZ(p.get<double>("GeneratorZ")) {
 
   fParticleTypes = p.get<std::vector<std::string>>("ParticleTypes");
+  produces<std::vector<simb::MCTruth>>();
 
 }
 
 void PDSPDataDrivenBeam::produce(art::Event& e) {
 
+  // Define the truth collection for this event.
+  auto mcTruths = std::make_unique<std::vector<simb::MCTruth>>();
+  simb::MCTruth truth;
+  mcTruths->push_back(truth);
+  e.put(std::move(mcTruths));
+  
+  std::cout << fCurrentEvent << " " <<
+               fRandPDG[fCurrentEvent] << " " <<
+               fRandMomentum[fCurrentEvent] << " " <<
+               fRandHUpstream[fCurrentEvent] << " " <<
+               fRandVUpstream[fCurrentEvent] << " " <<
+               fRandHDownstream[fCurrentEvent] << " " <<
+               fRandVDownstream[fCurrentEvent] << std::endl; 
+
+  fOutputPDG = fRandPDG[fCurrentEvent];
+  fOutputMomentum = fRandMomentum[fCurrentEvent];
+  fOutputHUpstream = fRandHUpstream[fCurrentEvent];
+  fOutputVUpstream = fRandVUpstream[fCurrentEvent];
+  fOutputHDownstream = fRandHDownstream[fCurrentEvent];
+  fOutputVDownstream = fRandVDownstream[fCurrentEvent];
+  std::cout << fOutputPDG << std::endl;
+  fOutputTree->Fill();
+
+  GenerateTrueEvent(truth);
+  ++fCurrentEvent;
 }
 
 void PDSPDataDrivenBeam::beginJob() {
+  art::ServiceHandle<art::TFileService> tfs;
+
   //Open input file
   fInputFile = new TFile(fFileName.c_str(), "READ");
 
   //Get all the PDFs  
-  //These will be a 1D distributions in momentum for each
-  //4D monitor bin (x,y for up/downstream)
-  //Stored in directories according to their particle type
   //
-  //
-  //Thinking of moving this to the following variables:
-  //
+  //Move to:
   //horiz/vert fibers in last XBPF
   //Px, Py, Pz -- or P, theta, phi (I like this more I think)
   //
@@ -101,27 +171,47 @@ void PDSPDataDrivenBeam::beginJob() {
   //The momentum is a bit tricky: we need to sample in 
   //reco space, but generate in true space. We'll have to 
   //unfold from the reco momentum in the spectrometer to 
-  //the true energy (taken from nominal MC) at the generation 
+  //the true energy (taken from nominal MC?) at the generation 
   //point (what is called the face in the beam sim files I think)
+
   for (size_t i = 0; i < fParticleTypes.size(); ++i) {
-    std::cout << "Fetching directory for " << fParticleTypes[i] << std::endl;
-    TDirectory * current_dir = (TDirectory*)fInputFile->Get(fParticleTypes[i].c_str());
-    TList * hists = (TList*)current_dir->GetListOfKeys();
-    for (int j = 0; j < hists->GetSize(); ++j) {
-      std::cout << hists->At(j)->GetName() << std::endl;
-      std::string title = hists->At(j)->GetTitle();
-      std::string h_bins = title.substr(0, title.find("_"));
-      std::string v_bins = title.substr(title.find("_")+1, title.size()-1);
-      std::cout << title << " " << h_bins << " " << v_bins << std::endl;
-    }
+    std::string part_type = fParticleTypes[i];
+    fPDFs[part_type] = (THnSparseD*)fInputFile->Get(part_type.c_str());
+
+    std::string n_part_type = "n" + part_type; 
+    TVectorD * n_parts = (TVectorD*)fInputFile->Get(n_part_type.c_str());
+    fParticleNums[part_type] = (*n_parts)[0];
+
+    fTotalParticles += (*n_parts)[0];
+  }
+
+  //Get the kinematic ranges from 1D projections of the first PDF (arbitrary)
+  THnSparseD * temp_pdf = fPDFs.begin()->second;
+  double minima[5];
+  double maxima[5];
+  for (size_t i = 0; i < 5; ++i) {
+    TH1D * temp_hist = (TH1D*)temp_pdf->Projection(i);
+    minima[i] = temp_hist->GetXaxis()->GetXmin();
+    maxima[i] = temp_hist->GetXaxis()->GetXmax();
+
+    //Free the memory used for the projection
+    delete temp_hist;
+  }
+  
+  //Compute fractions
+  double running_total = 0.;
+  for (auto it = fParticleNums.begin(); it != fParticleNums.end(); ++it) {
+    running_total += it->second / fTotalParticles;
+    fParticleFracs[it->first] = running_total;
   }
 
   //Will need to throw some random numbers
+  //Flat for pdg type
   //Flat in reco momentum
   //Flat in theta, phi, vert/horiz fibers
   //     --- Establish valid ranges?
   //
-  //Then for each of these, throw flat(0,1) 
+  //Then throw flat(0,1) 
   //and check against PDF in that 5D bin
   //
   //
@@ -134,9 +224,204 @@ void PDSPDataDrivenBeam::beginJob() {
   //I think we can do this all in the beginJob step, 
   //where we throw all of the numbers until we get 
   //N events, where N is a fcl parameter describing how
-  //many events we would like to generator. Save the 
+  //many events we would like to generate. Save the 
   //kinematics + position for these N events and then use
   //in the produce function to create the events. 
+  int nSampled = 0;
+  double kin_samples[5]; //the point in phase space to check against pdf
+  double pdf_check; //the number used for the checking
+  double pdg_sample = 0.; //point to sample fractions of particle types
+  while (nSampled < fNGenerate) {
+    pdg_sample = fRNG.Rndm();
+    //Get the pdg from the random num   
+    std::string part_type = "";
+    for (auto it = fParticleFracs.begin(); it != fParticleFracs.end(); ++it) {
+      if (pdg_sample <= it->second) {
+        if (fVerbose) {
+          std::cout << "Sampled " << it->first << " " <<
+                       pdg_sample << std::endl;
+        }
+        fRandPDG.push_back(fNameToPDG[it->first]);
+        part_type = it->first;
+        break;
+      }
+    }
+    
+    bool sample_again = true;
+    while (sample_again) {
+      fRNG.RndmArray(5, &kin_samples[0]);
+      pdf_check = fRNG.Rndm();  
+
+      //Need to convert the numbers sampled for the kinematics (0, 1) 
+      //to within the sampling range
+      double kin_point[5];
+      Convert(kin_samples, minima, maxima, kin_point);
+                                                //false: do not allocate a bin
+                                                //to save on memory usage
+      long long bin = fPDFs[part_type]->GetBin(&kin_point[0], false);
+      if (bin == -1) continue;
+      double pdf_value = fPDFs[part_type]->GetBinContent(bin);
+
+      if (pdf_check <= pdf_value) {
+        if (fVerbose) {
+          std::cout << "bin: " << bin << " PDF val: " << pdf_value << " Check: " << pdf_check << std::endl;
+          std::cout << kin_samples[0] << " " << kin_samples[1] << " " <<
+                       kin_samples[2] << " " << kin_samples[3] << " " <<
+                       kin_samples[4] << std::endl;
+          std::cout << kin_point[0] << " " << kin_point[1] << " " <<
+                       kin_point[2] << " " << kin_point[3] << " " <<
+                       kin_point[4] << std::endl;
+
+
+          std::cout << "Will keep" << std::endl;
+        }
+
+        fRandMomentum.push_back(kin_point[0]);
+        fRandVUpstream.push_back(int(kin_point[1]));
+        fRandHUpstream.push_back(int(kin_point[2]));
+        fRandVDownstream.push_back(int(kin_point[3]));
+        fRandHDownstream.push_back(int(kin_point[4]));
+
+        ++nSampled;
+        sample_again = false;
+      }
+    }
+  }
+
+  fOutputTree = tfs->make<TTree>("tree", ""); 
+  fOutputTree->Branch("PDG", &fOutputPDG);
+  fOutputTree->Branch("Event", &fCurrentEvent);
+  fOutputTree->Branch("Momentum", &fOutputMomentum);
+  fOutputTree->Branch("HUpstream", &fOutputHUpstream);
+  fOutputTree->Branch("VUpstream", &fOutputVUpstream);
+  fOutputTree->Branch("HDownstream", &fOutputHDownstream);
+  fOutputTree->Branch("VDownstream", &fOutputVDownstream);
+
+
+  //Setting up for positioning stuff
+  BeamMonitorBasisVectors();
+
+}
+
+void PDSPDataDrivenBeam::Convert(
+    double input_point[5], double minima[5],
+    double maxima[5], double output_point[5]) {
+
+  for (int i = 0; i < 5; ++i) {
+    double delta = maxima[i] - minima[i];
+    output_point[i] = minima[i] + delta * input_point[i];
+  }
+  
+}
+
+void PDSPDataDrivenBeam::GenerateTrueEvent(simb::MCTruth &mcTruth) {
+
+  MakePositionVector();
+/*
+  mcTruth.SetOrigin(simb::kSingleParticle);
+
+  std::string process = "primary";
+  int trackID = 0; //Change?
+
+  TLorentzVector position(0, 0, 0, 0);
+  TLorentzVector momentum(0., 0., 1., 1.);
+
+  simb::MCParticle newParticle(trackID, fCurrentPDG[fCurrentEvent], process);
+  newParticle.AddTrajectoryPoint(position, momentum);
+  
+  // Add the MCParticle to the MCTruth for the event.
+  mcTruth.Add(newParticle);
+  */
+}
+
+/*
+TLorentzVector PDSPDataDrivenBeam::MakeMomentumVector() {
+
+}
+*/
+
+TLorentzVector PDSPDataDrivenBeam::MakePositionVector() {
+  //First, get the vector between the two sets of tracking monitors
+  //
+  //Shift to center of fibers. Middle is 96
+  double upstreamX = 96. - int(fRandHUpstream[fCurrentEvent]) - .5;
+  double upstreamY = 96. - int(fRandVUpstream[fCurrentEvent]) - .5;
+  TVector3 upstream_point = ConvertProfCoordinates(upstreamX, upstreamY, 0., 
+                                                   fUpstreamZ);
+
+  std::cout << upstreamX << " " << upstreamY << std::endl;
+
+  double downstreamX = 96. - int(fRandHDownstream[fCurrentEvent]) - .5;
+  double downstreamY = 96. - int(fRandVDownstream[fCurrentEvent]) - .5;
+  TVector3 downstream_point = ConvertProfCoordinates(downstreamX, downstreamY, 0., 
+                                                     fDownstreamZ);
+
+  std::cout << downstreamX << " " << downstreamY << std::endl;
+
+  TVector3 diff = downstream_point - upstream_point;
+  TVector3 dR = diff.Unit();
+
+  std::cout << upstream_point.X() << " " << upstream_point.Y() << " " <<
+               upstream_point.Z() << std::endl;
+  std::cout << downstream_point.X() << " " << downstream_point.Y() << " " <<
+               downstream_point.Z() << std::endl;
+  std::cout << dR.X() << " " << dR.Y() << " " << dR.Z() << std::endl;
+
+  double deltaZ = (fGeneratorZ - downstream_point.Z());
+  double deltaX = (dR.X() / dR.Z()) * deltaZ; 
+  double deltaY = (dR.Y() / dR.Z()) * deltaZ; 
+
+  TVector3 generator_point = downstream_point +
+                             TVector3(deltaX, deltaY, deltaZ);
+  
+  std::cout << generator_point.X() << " " <<
+               generator_point.Y() << " " <<
+               generator_point.Z() << std::endl;
+
+  deltaZ = (-1.*fGeneratorZ);
+  deltaX = (dR.X() / dR.Z()) * deltaZ; 
+  deltaY = (dR.Y() / dR.Z()) * deltaZ; 
+
+  TVector3 last_point = generator_point +
+                        TVector3(deltaX, deltaY, deltaZ);
+
+  std::cout << last_point.X() << " " << last_point.Y() << " " <<
+               last_point.Z() << std::endl;
+
+  TVector3 momentum = fRandMomentum[fCurrentEvent]*dR;
+  std::cout << dR.X() << " " << dR.Y() << " " << dR.Z() << std::endl;
+
+  return TLorentzVector(0., 0., 0., 0.);
+}
+
+TVector3 PDSPDataDrivenBeam::ConvertProfCoordinates(
+    double x, double y, double z, double zOffset) {
+
+  double off = fNP04FrontZ - zOffset;
+
+  TVector3 old(x,y,z);
+
+  double newX = x*fBMBasisX.X() + y*fBMBasisY.X() + off*fabs(fBMBasisZ.X());
+  double newY = x*fBMBasisX.Y() + y*fBMBasisY.Y() + off*fabs(fBMBasisZ.Y());
+  double newZ = x*fBMBasisX.Z() + y*fBMBasisY.Z() - off*fabs(fBMBasisZ.Z());
+
+  newX += fBeamX*10.;
+  newY += fBeamY*10.;
+  newZ += fBeamZ*10.;
+
+  TVector3 result(newX/10., newY/10., newZ/10.);
+  return result;
+}
+
+void PDSPDataDrivenBeam::BeamMonitorBasisVectors(){
+  RotateMonitorVector(fBMBasisX);
+  RotateMonitorVector(fBMBasisY);
+  RotateMonitorVector(fBMBasisZ);
+}
+
+void PDSPDataDrivenBeam::RotateMonitorVector(TVector3 &vec){
+  vec.RotateX( fRotateMonitorYZ * TMath::Pi()/180. );
+  vec.RotateY( fRotateMonitorXZ * TMath::Pi()/180. );
 }
 
 DEFINE_ART_MODULE(PDSPDataDrivenBeam)
---
+++ dunesim/EventGenerator/make_THn_beam_input.py
@@ -0,0 +1,103 @@
+import ROOT as RT
+from array import array 
+from argparse import ArgumentParser as ap
+parser = ap()
+
+parser.add_argument( "-i", type=str, help='Input file', default="")
+parser.add_argument( "-o", type=str, help='Output file', default="beam_pdf.root")
+parser.add_argument( "-n", type=int, help='Max number of entries', default=-1)
+args = parser.parse_args()
+
+
+if not args.i:
+  exit()
+
+inputFile = RT.TFile(args.i, "OPEN")
+tree = inputFile.Get("beamreco/tree")
+
+''' fibers_h_upstream = NULL
+ fibers_v_upstream = NULL
+  fibers_h_downstream = NULL
+   fibers_v_downstream = NULL
+'''
+
+outputFile = RT.TFile(args.o, "RECREATE")
+                   #p,  fvu, fhu, fvd, fhd
+nBins = array("i", [25, 32, 32, 32, 32])
+mins = array("d", [0., 0., 0., 0., 0.]) 
+maxes = array("d", [2., 192., 192., 192., 192.]) 
+
+Pions = RT.THnSparseD("Pions", "", 5, nBins, mins, maxes)
+Protons = RT.THnSparseD("Protons", "", 5, nBins, mins, maxes)
+Electrons = RT.THnSparseD("Electrons", "", 5, nBins, mins, maxes)
+#kaon?
+
+counter = 0
+nProtons = 0
+nPions = 0
+nElectrons = 0
+#kaon?
+
+if args.n < 0:
+  max_entries = tree.GetEntries()
+else:
+  max_entries = args.n
+
+for e in tree:
+  if counter >= max_entries: break
+  f_v_up = [i for i in e.fibers_v_upstream]
+  f_h_up = [i for i in e.fibers_h_upstream]
+  f_v_down = [i for i in e.fibers_v_downstream]
+  f_h_down = [i for i in e.fibers_h_downstream]
+  perfectP = e.perfectP
+  Momentum = e.Momentum
+  pdgs = [i for i in e.possible_pdg]
+ 
+
+  if not (perfectP and len(f_v_up) == 1 and len(f_h_up) == 1 and
+          len(f_v_down) == 1 and len(f_h_down) == 1 and len(pdgs) > 0):
+    continue
+  
+  if Momentum < 0. or Momentum > 2.: continue 
+  
+  data = array("d", [Momentum, f_v_up[0], f_h_up[0], f_v_down[0], f_h_down[0]])
+
+  if pdgs[0] == 2212:
+    Protons.Fill(data)
+    nProtons += 1
+  elif pdgs[0] == 13:
+    Pions.Fill(data)   
+    nPions += 1
+  elif pdgs[0] == 11:
+    Electrons.Fill(data)
+    nElectrons += 1
+  #else kaon?
+
+  counter += 1 
+
+outputFile.cd()
+
+Pions.Scale(1. / nPions)
+Pions.Write()
+
+Protons.Scale(1. / nProtons)
+Protons.Write()
+
+if nElectrons > 0: Electrons.Scale(1. / nElectrons)
+Electrons.Write()
+
+##Saving the number of particles
+nElec = RT.TVectorD(1)
+nElec[0] += nElectrons
+nElec.Write("nElectrons")
+
+nProt = RT.TVectorD(1)
+nProt[0] += nProtons
+nProt.Write("nProtons")
+
+nPi = RT.TVectorD(1)
+nPi[0] += nPions
+nPi.Write("nPions")
+###############################
+
+outputFile.Close()
