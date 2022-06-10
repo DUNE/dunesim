a37e1692be77eb5b2508784c7c7e8acd5d16bef3
Michael Wang <mwang@fnal.gov>
2022-06-10 08:08:51 -0500

    new files to allow unique noise per channel & to save pure signal waveform; updated NuEScatterGen to allow custom BRs and flat spectrum and added James Shen's fixes

---
+++ dunesim/DetSim/Module/SimWireClnSigDUNE_module.cc
@@ -0,0 +1,365 @@
+// SimWireClnSigDUNE_module.cc (modified version of SimWireDUNE_module.cc,
+//
+// David Adams
+// December 2015
+//
+// SimWireDUNE class designed to simulate signal on a wire in the TPC
+//
+// Developed from the now-obsolete SimWireDUNE35t_module.cc. This implementation
+// follows the TSI model where most of the algorithmic code is moved to
+// services accessed via service interfaces.
+//
+// For configuration parameters, see the "Flags" block in the module class
+// definition below. Remove the leading "f" to get the parameter name.
+//
+// There is no flag for compression because this must always be invoked
+// to apply zero suppression. Use ReplaceCompressService (prolog cmpreplace)
+// to effectively skip the compression while retaining the supression.
+//
+// The interface names for the accessed services are listed in the "Services"
+// block in the header below.
+//
+// Some useful module and service configurations may be found in
+// detsimmodules_dune.fcl, e.g. cmpreplace to skip compression.
+//
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//
+// Mike Wang
+// September 2021
+//
+// This version (SimWireClnSigDUNE) keeps both the signal+noise and pure signal
+// waveforms. For this version, please do the following in the fcl file:
+//
+// (a) set the NoiseScale for the PedestalAdditionService to 0
+// (b) set DistortOn in the daq section to "false"
+// (c) remember to set fSuppressUsingClnSig to true if doing zero suppresion
+//     based on clean signal
+//
+
+#include <vector>
+#include <string>
+#include <sstream>
+#include <iostream>
+
+#include "art/Framework/Core/ModuleMacros.h"
+#include "art/Framework/Core/EDProducer.h"
+#include "art/Framework/Principal/Event.h"
+#include "art/Framework/Principal/Handle.h"
+#include "art/Framework/Services/Registry/ServiceHandle.h"
+#include "fhiclcpp/ParameterSet.h"
+#include "messagefacility/MessageLogger/MessageLogger.h"
+
+#include "lardataobj/RawData/raw.h"
+#include "larcore/Geometry/Geometry.h"
+#include "lardataobj/Simulation/sim.h"
+#include "lardataobj/Simulation/SimChannel.h"
+#include "lardataobj/RawData/RawDigit.h"
+#include "lardata/DetectorInfoServices/DetectorPropertiesService.h"
+
+#include "dunecore/ArtSupport/DuneToolManager.h"
+
+#include "dunecore/DuneInterface/Data/AdcTypes.h"
+#include "dunecore/DuneInterface/Data/AdcSimulator.h"
+#include "dunecore/DuneInterface/Service/AdcSuppressService.h"
+#include "dunecore/DuneInterface/Service/AdcCompressService.h"
+#include "dunecore/DuneInterface/Service/SimChannelExtractService.h"
+#include "dunecore/DuneInterface/Service/UniqChannelNoiseService.h"
+#include "dunecore/DuneInterface/Service/PedestalAdditionService.h"
+#include "dunecore/DuneInterface/Service/AdcDistortionService.h"
+
+using std::ostringstream;
+using std::cout;
+using std::endl;
+using std::string;
+
+//**********************************************************************
+
+// Base class for creation of raw signals on wires.
+class SimWireClnSigDUNE : public art::EDProducer {
+
+public:
+
+  explicit SimWireClnSigDUNE(fhicl::ParameterSet const& pset);
+  virtual ~SimWireClnSigDUNE();
+
+  // read/write access to event
+  void produce (art::Event& evt);
+  void beginJob();
+  void endJob();
+  void reconfigure(fhicl::ParameterSet const& p);
+
+private:
+
+  std::string fSimChannelLabel; ///< Data product holding the ionization electrons
+
+  // Flags.
+  bool fNoiseOn;           ///< noise turned on or off for debugging; default is on
+  bool fPedestalOn;        ///< switch for simulation of nonzero pedestals
+  bool fDistortOn;         ///< switch for simulation of stuck bits
+  bool fSuppressOn;        ///< switch for simulation of zero suppression
+  bool fSuppressUsingClnSig; ///< do zs based on clean signal
+  bool fKeepEmptyChannels; ///< Write out empty channels iff true.
+  // Tools.
+  std::string fAdcSimulatorName;
+
+
+  std::unique_ptr<AdcSimulator> m_pads;
+
+  // Services.
+  art::ServiceHandle<geo::Geometry> m_pgeo;
+  art::ServiceHandle<AdcSuppressService> m_pzs;
+  art::ServiceHandle<AdcCompressService> m_pcmp;
+  art::ServiceHandle<SimChannelExtractService> m_pscx;
+  art::ServiceHandle<UniqChannelNoiseService> m_pcns;
+  art::ServiceHandle<AdcDistortionService> m_pdis;
+  art::ServiceHandle<PedestalAdditionService> m_ppad;
+
+}; // class SimWireClnSigDUNE
+
+DEFINE_ART_MODULE(SimWireClnSigDUNE)
+
+//**********************************************************************
+
+SimWireClnSigDUNE::SimWireClnSigDUNE(fhicl::ParameterSet const& pset) : EDProducer{pset} {
+  reconfigure(pset);
+  produces< std::vector<raw::RawDigit> >();
+  produces< std::vector<raw::RawDigit> >("signal");
+}
+
+//**********************************************************************
+
+SimWireClnSigDUNE::~SimWireClnSigDUNE() { }
+
+//**********************************************************************
+
+void SimWireClnSigDUNE::reconfigure(fhicl::ParameterSet const& p) {
+  string myname = "SimWireClnSigDUNE::reconfigure: ";
+  string myprefix = myname + "    ";
+  fSimChannelLabel   = p.get<std::string>("SimChannelLabel");
+  fNoiseOn           = p.get<bool>("NoiseOn");
+  fPedestalOn        = p.get<bool>("PedestalOn");
+  fDistortOn         = p.get<bool>("DistortOn");
+  fSuppressOn        = p.get<bool>("SuppressOn");
+  fSuppressUsingClnSig = p.get<bool>("SuppressUsingClnSig", false);
+  fKeepEmptyChannels = p.get<bool>("KeepEmptyChannels");
+  fAdcSimulatorName = p.get<string>("AdcSimulator");
+
+  DuneToolManager* pdtm = DuneToolManager::instance();
+  m_pads = pdtm == nullptr ? nullptr : pdtm->getPrivate<AdcSimulator>(fAdcSimulatorName);
+  ostringstream out;
+  out << myname << "Tools:" << endl;
+  out << "  AdcSimulator: " << bool(m_pads) << endl;
+  out << myname << "Accessed services:" << endl;
+  out << myname << "  SimChannel extraction service:" << endl;
+  m_pscx->print(out, myprefix) << endl;
+  if ( fNoiseOn ) {
+    out << myname << "  Channel noise service:" << endl;;
+    m_pcns->print(out, myprefix);
+  } else {
+    out << myname << "  Channel noise is off.";
+  }
+  out << endl;
+  if ( fPedestalOn ) {
+    out << myname << "  Pedestal addition service:" << endl;;
+    m_ppad->print(out, myprefix);
+  } else {
+    out << myname << "  Pedestal addition is off.";
+  }
+  out << endl;
+  if ( fDistortOn ) {
+    out << myname << "  ADC distortion service:" << endl;;
+    m_pdis->print(out, myprefix);
+  } else {
+    out << myname << "  ADC distortion is off.";
+  }
+  out << endl;
+  if ( fSuppressOn ) {
+    out << myname << "  ADC suppression service:" << endl;
+    m_pzs->print(out, myprefix);
+  } else {
+    out << myname << "  ADC suppression is off.";
+  }
+  out << endl;
+  out << myname << "  Compression service:" << endl;
+  out << endl;
+  m_pcmp->print(out, myprefix);
+  out << myname << "  KeepEmptyChannels:" << fKeepEmptyChannels << endl;
+  mf::LogInfo("SimWireClnSigDUNE::reconfigure") << out.str();
+
+  return;
+}
+
+//**********************************************************************
+
+void SimWireClnSigDUNE::beginJob() { }
+
+//**********************************************************************
+
+void SimWireClnSigDUNE::endJob() { }
+
+//**********************************************************************
+
+void SimWireClnSigDUNE::produce(art::Event& evt) {
+  const string myname = "SimWireClnSigDUNE::produce: ";
+
+  // Unlike in the standard SimWireDUNE_module, in this "ClnSig"
+  // version, we do not create a vector of sim::SimChannel* that
+  // has the same number of entries as the number of detector 
+  // channels.  This is because we only loop through SimChannels
+  // that have signal in them.
+  std::vector<const sim::SimChannel*> chanHandle;
+  std::map<unsigned int, unsigned int> WGrp2NoiseMap;
+  unsigned int ncohnoise = 0;
+  evt.getView(fSimChannelLabel, chanHandle);
+  for ( size_t c=0; c<chanHandle.size(); ++c ) {
+    unsigned int wgrp = m_pcns->getGroupNumberFromOfflineChannel(chanHandle[c]->Channel());
+    auto [itwgn, newpair ] = WGrp2NoiseMap.insert(std::map<unsigned int, unsigned int>::value_type(wgrp, ncohnoise));
+    if ( newpair ) ncohnoise++;
+  }
+  // Let channel noise service know how many array points to generate
+  m_pcns->setNumNoiseArrayPoints(chanHandle.size(), ncohnoise);
+
+  // make an unique_ptr of sim::SimDigits that allows ownership of the produced
+  // digits to be transferred to the art::Event after the put statement below
+  std::unique_ptr<std::vector<raw::RawDigit>>  digcol(new std::vector<raw::RawDigit>);
+  std::unique_ptr<std::vector<raw::RawDigit>>  digcol2(new std::vector<raw::RawDigit>);
+
+  // Fetch the number of ticks to write out for each channel.
+  auto const clockData = art::ServiceHandle<detinfo::DetectorClocksService const>()->DataFor(evt);
+  auto const detProp = art::ServiceHandle<detinfo::DetectorPropertiesService const>()->DataFor(evt, clockData);
+  unsigned int nTickReadout  = detProp.ReadOutWindowSize();
+  m_pcns->newEvent(); // all unique noise waveforms will be pre-generated here
+  // Loop over SimChannels (i.e. ignore channels with no deposited charge).
+  std::map<int,double>::iterator mapIter;
+  for ( size_t c=0; c<chanHandle.size(); ++c ) {
+
+    // Get the SimChannel for this channel
+    const sim::SimChannel* psc = chanHandle[c];
+    unsigned int chan = chanHandle[c]->Channel();
+
+    // Create vector that holds the floating ADC count for each tick.
+    std::vector<AdcSignal> fChargeWork;
+
+    // Extract the floating ADC count from the SimChannel for each tick in the channel.
+    m_pscx->extract(clockData, psc, fChargeWork);
+
+    // Make a copy to store pure signal
+    std::vector<AdcSignal> fChargeWork2(fChargeWork);
+
+    // Add noise to each tick in the channel.
+    if ( fNoiseOn ) {
+      unsigned int wgrp = m_pcns->getGroupNumberFromOfflineChannel(chan);
+      m_pcns->setIdxNoiseArrayPoints(c, WGrp2NoiseMap[wgrp]);
+      m_pcns->addNoise(clockData, detProp, chan, fChargeWork);
+    }
+
+    // Option to display signals before adding pedestals.
+    // E.g. logsig = chan==1000.
+    bool logsig = false;
+    if ( logsig ) {
+      cout << myname << "Signals after noise:" << endl;
+      for ( unsigned int itck=0; itck<fChargeWork.size(); ++itck ) {
+        if(fChargeWork[itck] > 0 )
+          cout << myname << " " << itck << ": chg=" << fChargeWork[itck] << endl;
+      }
+    }
+
+    // Add pedestal.
+    float pedval = 0.0; // Pedestal to be recorded in RawDigits collection
+    float pedrms = 0.0; // Pedestal RMS to be recorded in RawDigits collection
+    if ( fPedestalOn ) {
+      m_ppad->addPedestal(chan, fChargeWork, pedval, pedrms);
+      m_ppad->addPedestal(chan, fChargeWork2, pedval, pedrms);
+    }
+
+    // Convert floating ADC to integral ADC count.
+    std::vector<short> adcvec(fChargeWork.size(), 0);
+    std::vector<short> adcvec2(fChargeWork2.size(), 0);
+    const short adcmax = 4095;
+    for ( unsigned int itck=0; itck<fChargeWork.size(); ++itck ) {
+
+      AdcSignal adcin = fChargeWork[itck];
+      AdcSignal adcin2 = fChargeWork2[itck];
+      short adc = 0;
+      short adc2 = 0;
+      bool useOldAdc = false;
+      // New ADC calculation (with tool).
+      if ( m_pads ) {
+        if(adcin > 0)
+          //cout << " adcin " << adcin << endl;
+        adc = m_pads->count(adcin, chan);
+        adc2 = m_pads->count(adcin2, chan);
+      } else {
+        //cout << myname << "WARNING: AdcSimulator not found." << endl;
+        useOldAdc = true;
+      }
+      // Old ADC calculation.
+      if ( useOldAdc ) {
+        short adci = 0;
+        if ( adcin > 0 ) adci = (short) (adcin + 0.5);
+        if ( adci > adcmax ) adci = adcmax;
+        bool show = m_pads && adci != adc;
+
+        static int ndump = 1000;
+        if ( ndump && show ) {
+          cout << myname << "  ADC: " << adci << " --> " << adc << " (" << adcin << ")" << endl;
+          --ndump;
+        }
+        adc = adci;
+
+        // do the same for pure signal
+        adci = 0;
+        if ( adcin2 > 0 ) adci = (short) (adcin2 + 0.5);
+        if ( adci > adcmax ) adci = adcmax;
+        adc2 = adci;
+      }
+      // Record the ADC value.
+      adcvec[itck] = adc;
+      adcvec2[itck] = adc2;
+    }
+    // Resize to the correct number of time samples, dropping extra samples.
+    adcvec.resize(nTickReadout);
+    adcvec2.resize(nTickReadout);
+
+    // Add stuck bits.
+    if ( fDistortOn ) {
+      m_pdis->modify(chan, adcvec);
+    }
+
+    // Zero suppress.
+    std::vector<short> vadc;
+    if ( fSuppressUsingClnSig ) {   
+      vadc = adcvec2;
+    } else {
+      vadc = adcvec;
+    }
+    AdcCountSelection acs(vadc, chan, pedval);
+    if ( fSuppressOn ) {
+      m_pzs->filter(acs);
+    }
+    int nkeep = 0;
+    for ( bool kept : acs.filter ) if ( kept ) ++nkeep;
+
+    // If flag is not set and channel is empty, skip it.
+    if ( ! fKeepEmptyChannels && nkeep==0 ) continue;
+
+    // Compress.
+    raw::Compress_t comp = raw::kNone;
+    m_pcmp->compress(adcvec, acs.filter, pedval, comp);
+    m_pcmp->compress(adcvec2, acs.filter, pedval, comp);
+
+    // Create and store raw digit.
+    raw::RawDigit rd(chan, nTickReadout, adcvec, comp);
+    rd.SetPedestal(pedval, pedrms);
+    digcol->push_back(rd);            // add this digit to the collection
+
+    raw::RawDigit rd2(chan, nTickReadout, adcvec2, comp);
+    rd2.SetPedestal(pedval, pedrms);
+    digcol2->push_back(rd2);            // add this digit to the collection
+
+  }  // end loop over channels
+
+  evt.put(std::move(digcol));
+  evt.put(std::move(digcol2),"signal");
+
+}
